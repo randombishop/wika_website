95523535ffbf2647318c85c0abf86b983b9f2427
Nabil Abdellaoui <nabil.abdellaoui@tala.co>
2021-07-04 16:17:42 -0700

    Deploy website - based on 110ed26454aae7e4f48f011fd2054cf475c98fa6

---
+++ assets/js/814f3328.c74ffcc2.js
@@ -0,0 +1,1 @@
+(self.webpackChunkwika=self.webpackChunkwika||[]).push([[535],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/wika_website/blog/welcome"},{"title":"Hello","permalink":"/wika_website/blog/hello-world"},{"title":"Hola","permalink":"/wika_website/blog/hola"}]}')}}]);
