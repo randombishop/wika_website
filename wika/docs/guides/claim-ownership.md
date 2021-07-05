7a0de0e9a8a456c2978f359fd0e60328d639f617
Nabil Abdellaoui <nabil.abdellaoui@expr-research.com>
2021-07-05 08:45:31 -0700

    Docs1 (#2)
    * roadmap
    
    * background
    
    * background
    
    Co-authored-by: Nabil Abdellaoui <nabil.abdellaoui@tala.co>

--- wika/docs/guides/claim-ownership.md
+++ wika/docs/guides/claim-ownership.md
@@ -4,3 +4,95 @@ sidebar_position: 3
 
 # Claim Ownership
 
+## 1. Copy your ownership mark
+
+- Open the Wika Extension.
+
+- Go to menu > Claim Page Ownership
+
+- Click on the button **1. Copy This** to copy the mark that will prove you own the page.
+
+![screenshot](/img/docs/guides/claim_ownership.png)
+
+- The mark should look like this:
+```wika.network/author/0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d```
+
+- The long string after author/ is actually your Wika address in raw format, 
+and you can use the same one for all the pages that you own.
+
+
+## 2. Add the mark to your page
+
+### 2.a YouTube example
+
+- Go to your YouTube account videos and click on the edit icon.
+
+![screenshot](/img/docs/guides/edit_youtube_video.png)
+
+- Scroll down, click "Show More" and find the **Tags** input
+
+- Add the mark you copied as a tag.
+
+![screenshot](/img/docs/guides/tag_youtube_video.png)
+
+- Save the change and close.
+
+
+### 2.b Medium example
+
+- Medium doesn't provide a way to attach a tag to the webpage like youtube, 
+so we're going to use a trick to add the mark to the page: we're going to attach it to an image.
+
+- Go to your Medium account and edit your story.
+
+- If you have an image already in your article, click on the image, then 
+
+![screenshot](/img/docs/guides/medium_image_example1.png)
+
+- Paste the copied mark as the image alternative text
+
+![screenshot](/img/docs/guides/medium_image_example2.png)
+
+- Save and close your article.
+
+
+### 2.c Other ways to add the mark
+
+There are several other ways you can add the mark in your page, as a link, page metadata, or any hidden or visible
+part of the page.
+
+As long as the mark appears somewhere in the page, the blockchain will be able to validate your ownership.
+
+Please contact Wika team in discord channel if you're having trouble adding your mark.
+
+
+
+## 3. Submit your ownership verification request
+
+- Navigate to your page using the same URL that will be used by any other person.
+
+- Open the Wika extension.
+
+- Go to menu > Claim Page Ownership.
+
+- Click on "Submit your request"
+
+- You'll have to wait about 2 minutes for the blockchain to validate your request
+ and gather the results from different distributed verifiers.
+ 
+- Once done, your request should be approved and the extension should show that the page belongs to you now
+
+![screenshot](/img/docs/guides/you_are_the_owner.png)
+
+
+## 4. Good to know
+
+- Congratulations, you are now the official owner of a webpage in the decentralized Wika registry!
+
+- Make sure you keep the mark in the page even after the verification process was complete. 
+The reason for this is that if someone can add a comment to the page, they could add their mark and claim ownership.
+
+- For the same reasons, make sure you add your mark in the beginning of the page rather than the end, 
+the blockchain considers the first mark it will find in the page as a valid proof.
+
+
--- wika/docusaurus.config.js
+++ wika/docusaurus.config.js
@@ -3,99 +3,98 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'Wika Network',
-  tagline: 'Make the internet a better place!',
-  url: 'https://wika_website.github.io',
-  baseUrl: '/wika_website/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'randombishop', // Usually your GitHub org/user name.
-  projectName: 'wika_website', // Usually your repo name.
-  themeConfig: {
-    colorMode: {
-      defaultMode: 'light',
-      disableSwitch: true
+    title: 'Wika Network',
+    tagline: 'Make the internet a better place!',
+    url: 'http://www.wika.network',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'randombishop', // Usually your GitHub org/user name.
+    projectName: 'wika_website', // Usually your repo name.
+    trailingSlash: false,
+    themeConfig: {
+        colorMode: {
+            defaultMode: 'light',
+            disableSwitch: true
+        },
+        navbar: {
+            title: 'Wika Network',
+            logo: {
+                alt: 'Wika Logo',
+                src: 'img/logo32.png',
+            },
+            items: [
+                {
+                    to: '/how-it-works',
+                    label: 'How it works',
+                    position: 'left'
+                },
+                {
+                    to: '/roadmap',
+                    label: 'Roadmap',
+                    position: 'left'
+                },
+                {
+                    type: 'doc',
+                    docId: 'intro',
+                    position: 'left',
+                    label: 'Documentation',
+                }
+            ]
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Github',
+                            href: 'https://github.com/randombishop',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discordapp.com/invite/sPEwJUmAwH',
+                        }
+                    ],
+                },
+                {
+                    title: 'Code',
+                    items: [
+                        {
+                            label: 'Browser Extension',
+                            href: 'https://github.com/randombishop/wika_browser_ext',
+                        },
+                        {
+                            label: 'Blockchain Node',
+                            href: 'https://github.com/randombishop/wika_node',
+                        },
+                        {
+                            label: 'This website',
+                            href: 'https://github.com/randombishop/wika_website',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Wika Network.`,
+        },
+        prism: {
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
+        },
     },
-    navbar: {
-      title: 'Wika Network',
-      logo: {
-        alt: 'Wika Logo',
-        src: 'img/logo32.png',
-      },
-      items: [
-        {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Documentation',
-        }
-      ]
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Community',
-          items: [
+    presets: [
+        [
+            '@docusaurus/preset-classic',
             {
-              label: 'Github',
-              href: 'https://github.com/randombishop',
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js')
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
             },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/sPEwJUmAwH',
-            }
-          ],
-        },
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'White paper',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Code',
-          items: [
-            {
-              label: 'Browser Extension',
-              href: 'https://github.com/randombishop/wika_browser_ext',
-            },
-            {
-              label: 'Blockchain Node',
-              href: 'https://github.com/randombishop/wika_node',
-            },
-            {
-              label: 'This website',
-              href: 'https://github.com/randombishop/wika_website',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Wika Network.`,
-    },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js')
-        },
-        blog: {
-          showReadingTime: true
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
+        ],
     ],
-  ],
 };
